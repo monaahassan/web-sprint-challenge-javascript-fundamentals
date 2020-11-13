894dc9bdd2913d3d894fbcef36a5ff6daccbb2ce
Mona Hassan <monamia@Monas-MacBook-Pro.local>
2020-11-13 12:46:55 -0600

    Finished

--- index.js
+++ index.js
@@ -173,8 +173,7 @@ console.log(cuboidTwo);
 // console.log(cuboidTwo.volume()); // 100
 // console.log(cuboidTwo.surfaceArea()); // 130
 
-
-
+ 
 
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
